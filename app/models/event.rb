f4aa26ffbe2243d307ac85049e6660a22bdd2c10
Kiichiro-T <tennis_0503@outlook.jp>
2019-12-19 13:27:15 +0900

    Transactionのバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many  :transactions
   validates :name, presence: true, length: { maximum: 1024 }
   validates :user_id, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,17 +1,27 @@
 class Transaction < ApplicationRecord
-  belongs_to :user
+  belongs_to :creditor, class_name: 'User', foreign_key: 'creditor_id'
+  belongs_to :debtor, class_name: 'User', foreign_key: 'debtor_id'
   belongs_to :event
-  validates :user_id, presence: true
+  belongs_to :group
+  validates :debtor_id, presence: true
+  validates :creditor_id, presence: true
   validates :event_id, presence: true
-  validates :debtor, presence: true
-  validate :deadline_before_today
+  validates :group_id, presence: true
+  validate  :deadline_before_today
   validates :debt, presence: true
   validates :debt, numericality: { only_integer: true}
   validates :debt, numericality: { greater_than_or_equal_to: 0 }
+  validates :payment, presence: true
+  validates :payment, numericality: { only_integer: true}
+  validates :payment, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
     def deadline_before_today
-      errors.add(:start_date, "は今日以降のものを選択してください") if deadline.nil? || deadline < Date.today.to_datetime
+      errors.add(:deadline, "は今日以降のものを選択してください") if deadline.nil? || deadline < Date.today.to_datetime
     end
+
+    # def payment_is_equal_or_smaller_than_debt
+    #   errors.add(:payment, "は", :debt, "以下にしてください") if payment <= debt
+    # end
 end
