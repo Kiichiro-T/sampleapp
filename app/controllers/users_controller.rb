b3e884790549a393e5a260949b9859874652f148
Kiichiro-T <tennis_0503@outlook.jp>
2020-01-09 16:05:09 +0900

    bootstrap用のフラッシュに変更した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,22 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.all
+  end 
+
+  def new
   end
 
   def batch
     if params[:file].blank?
-      flash[:error] = "読み込むファイルを選択してください"
+      flash[:danger] = "読み込むファイルを選択してください"
       redirect_to new_users_url
     elsif File.extname(params[:file].original_filename) != ".csv"
-      flash[:error] = "csvファイルのみ読み込み可能です"
+      flash[:danger] = "csvファイルのみ読み込み可能です"
       redirect_to new_users_url
     else
       group = Group.find_by(id: current_user.group_id)
       count = User.import(params[:file], group)
-      flash[:notice] = "#{count.to_s}人のユーザーを追加しました"
+      flash[:success] = "#{count.to_s}人のユーザーを追加しました"
       redirect_to users_url
     end
   end
