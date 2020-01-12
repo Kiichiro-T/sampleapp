df8628a54b8266ac0b8dcd92fd6ab1c2ac6f9d72
Kiichiro-T <tennis_0503@outlook.jp>
2020-01-12 23:25:52 +0900

    一つのユーザーに一つのグループに

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :confirm_definitive_registration
+  before_action :one_user_has_one_group, only: [:new, :create]
   def index
     @groups = Group.all
   end
@@ -29,6 +30,13 @@ class GroupsController < ApplicationController
 
     def group_params
       params.require(:group).permit(:name, :leader_id, :email)
+    end
+
+    def one_user_has_one_group
+      if current_user.group_id.present?
+        flash[:warning] = "１ユーザーにつき１グループなので作成できません"
+        redirect_to root_url
+      end
     end
 
     #def user_group_id_params
