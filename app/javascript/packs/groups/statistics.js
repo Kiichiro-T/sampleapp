742fa82ef36a09bff8beac6cce98d1ea2abb2620
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-05 18:49:12 +0900

    statisticsの当て込み

---
+++ app/javascript/packs/groups/statistics.js
@@ -0,0 +1,6 @@
+const images = require.context('../../images', true)
+
+import '../../javascripts/rails-ujs'
+import '../groups/statistics.css'
+import '../../src/shared/frame.css'
+import '../../src/shared/menu.css'
