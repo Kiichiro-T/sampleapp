97771534c3821d1b97a5f721a1cf4ce51e270115
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-09 16:36:24 +0900

    sidekiqの定時ジョブの管理画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
   # get 'receipt_pdfs/index', to: 'receipt_pdfs#index'
   devise_for :users, controllers: {
