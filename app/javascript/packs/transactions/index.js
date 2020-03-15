014b3927a9a95671d6c3b2b715f4c24108021e66
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-15 20:39:57 +0900

    My収支ページ

--- app/javascript/packs/transactions/index.js
+++ app/javascript/packs/transactions/index.js
@@ -1,1 +1,6 @@
+const images = require.context('../../images', true)
+
 import '../../javascripts/rails-ujs'
+import './index.css'
+import '../../src/shared/frame.css'
+import '../../src/shared/menu.css'
