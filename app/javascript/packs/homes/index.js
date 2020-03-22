1eec97576197670f85964cc40349ee8c8ef149dd
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-23 00:14:56 +0900

    chart.jsの設定

--- app/javascript/packs/homes/index.js
+++ app/javascript/packs/homes/index.js
@@ -1,6 +1,11 @@
+require("intro.js")
+require("chart.js")
 const images = require.context('../../images', true)
 
 import '../../javascripts/rails-ujs'
+import 'intro.js'
+import '../../javascripts/intro'
 import './index.css'
 import '../../src/shared/frame.css'
 import '../../src/shared/menu.css'
+import '../../src/shared/introjs.css'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,18 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
-environment.plugins.append(
-  'ProvidePlugin-IntroJS',
-  new webpack.ProvidePlugin({
-    introJs: ['intro.js', 'introJs']
-  })
-)
-
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    introJs: ['intro.js', 'introJs']
   })
 )
 
