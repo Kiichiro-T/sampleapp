f71d1fd0919fd65d0a9507988706b770bb95fcca
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-29 19:46:03 +0900

    ホームのコントローラーの変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,8 +5,8 @@ class HomesController < ApplicationController
   before_action :confirm_definitive_registration
   def index
     @my_groups = Group.my_groups(current_user)
-    events = Event.my_events(current_user).order(start_date: :desc)
-    @events = Kaminari.paginate_array(events).page(params[:page]).per(5)
+    today = Time.current.midnight
+    @events = Event.my_events(current_user).where('start_date >= ?', today).order(start_date: :asc).limit(5)
     @new_events = Event.my_events(current_user).where(created_at: (Time.now.midnight - 7.days)..Time.now.midnight).limit(5).order(created_at: :desc)
     # いずれは、Transaction.where("(creditor_id = ?) OR (debtor_id = ?)", user_id, user_id)
   end
