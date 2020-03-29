9f5afb2932343eed32c57fb46ada437967948588
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-29 20:05:16 +0900

    更新情報コンポーネントの作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,7 +7,7 @@ class HomesController < ApplicationController
     @my_groups = Group.my_groups(current_user)
     today = Time.current.midnight
     @events = Event.my_events(current_user).where('start_date >= ?', today).order(start_date: :asc).limit(5)
-    @new_events = Event.my_events(current_user).where(created_at: (Time.now.midnight - 7.days)..Time.now.midnight).limit(5).order(created_at: :desc)
+    @new_events = Event.my_events(current_user).where(created_at: (today - 7.days)..today.end_of_day).limit(4).order(created_at: :asc)
     # いずれは、Transaction.where("(creditor_id = ?) OR (debtor_id = ?)", user_id, user_id)
   end
 end
