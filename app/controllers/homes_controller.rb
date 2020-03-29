012783b549557073a24c4bb47e1d5ec86af89fb5
Kiichiro-T <tennis_0503@outlook.jp>
2020-03-29 18:34:00 +0900

    所属グループコンポーネントの作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
   before_action :confirm_definitive_registration
   def index
-    @groups = Group.my_groups(current_user)
+    @my_groups = Group.my_groups(current_user)
     events = Event.my_events(current_user).order(start_date: :desc)
     @events = Kaminari.paginate_array(events).page(params[:page]).per(5)
     @new_events = Event.my_events(current_user).where(created_at: (Time.now.midnight - 7.days)..Time.now.midnight).limit(5).order(created_at: :desc)
