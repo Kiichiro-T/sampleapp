5164b77907d2f0225d7aa6f910bce8eb09048334
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-01 21:14:39 +0900

    リファクタリング

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,70 +36,37 @@ class GroupsController < ApplicationController
   end
 
   def inheritable_search
-    keyword = params[:keyword]
-    user_ids = []
-    GroupUser.where(group_id: @group.id, role: GroupUser.roles[:general]).each do |relationship|
-      user_ids << User.find(relationship.user_id).id
-    end
-    members = User.where(id: user_ids)
-    @members = members.where('name LIKE :keyword OR furigana LIKE :keyword ', keyword: "%#{keyword.tr('ぁ-ん','ァ-ン')}%").order(furigana: :asc)
-    respond_to do |format|
-      format.json { render 'inheritable_members', json: @members }
-    end
+    incremental_search('inheritable_members')
   end
 
   def inherit
     @executives = User.executives(@group)
-    if params[:new_executive].present?
-      if new_executive.present?
-        executive_relationship = GroupUser.executive_relationship(group: @group, user: current_user)
-        general_relationship = GroupUser.general_relationship(group: @group, user: new_executive)
-        begin
-          GroupUser.transaction do
-            executive_relationship.update!(role: GroupUser.roles[:general])
-            general_relationship.update!(role: GroupUser.roles[:executive])
-          end
-          flash_and_redirect(key: :success, message: '引継ぎが成功しました', redirect_url: root_url)
-        rescue => e
-          ErrorUtility.log_and_notify(e)
-          flash_and_render(key: :danger, message: 'エラーが発生しました。', action: 'change')
-        end
-      else
-        flash_and_render(key: :danger, message: '選択した人はすでに他のグループの幹事です。本人にご確認ください', action: 'change')
-      end
+    return flash_and_render(key: :danger, message: '引継ぎたい人を選択してください', action: 'change') if params[:new_executive].blank?
+
+    return flash_and_render(key: :danger, message: '選択した人はすでに他のグループの幹事です。本人にご確認ください', action: 'change') if new_executive.blank?
+
+    if GroupUser.inherit(group: @group, current_user: current_user, new_executive: new_executive)
+      flash_and_redirect(key: :success, message: '引継ぎが成功しました', redirect_url: root_url)
     else
-      flash_and_render(key: :danger, message: '引継ぎたい人を選択してください', action: 'change')
+      flash_and_render(key: :danger, message: 'エラーが発生しました。', action: 'change')
     end
   end
 
   def assignable_search
-    keyword = params[:keyword]
-    user_ids = []
-    GroupUser.where(group_id: @group.id, role: GroupUser.roles[:general]).each do |relationship|
-      user_ids << User.find(relationship.user_id).id
-    end
-    members = User.where(id: user_ids)
-    @members = members.where('name LIKE :keyword OR furigana LIKE :keyword ', keyword: "%#{keyword.tr('ぁ-ん','ァ-ン')}%").order(furigana: :asc)
-    respond_to do |format|
-      format.json { render 'assignable_members', json: @members }
-    end
+    incremental_search('assignable_members')
   end
 
   def assign
     @executives = User.executives(@group)
-    if params[:new_executive].present?
-      if new_executive.present?
-        general_relationship = GroupUser.general_relationship(group: @group, user: new_executive)
-        if general_relationship.update(role: GroupUser.roles[:executive])
-          flash_and_redirect(key: :success, message: '任命に成功しました', redirect_url: root_url)
-        else
-          flash_and_render(key: :danger, message: 'エラーが発生しました。', action: 'change')
-        end
-      else
-        flash_and_render(key: :danger, message: '選択した人はすでに他のグループの幹事です。本人にご確認ください', action: 'change')
-      end
+    return flash_and_render(key: :danger, message: '任命したい人を選択してください', action: 'change') if params[:new_executive].blank?
+
+    return flash_and_render(key: :danger, message: '選択した人はすでに他のグループの幹事です。本人にご確認ください', action: 'change') if new_executive.blank?
+
+    general_relationship = GroupUser.general_relationship(group: @group, user: new_executive)
+    if general_relationship.update(role: GroupUser.roles[:executive])
+      flash_and_redirect(key: :success, message: '任命に成功しました', redirect_url: root_url)
     else
-      flash_and_render(key: :danger, message: '任命したい人を選択してください', action: 'change')
+      flash_and_render(key: :danger, message: 'エラーが発生しました。', action: 'change')
     end
   end
 
@@ -119,19 +86,20 @@ class GroupsController < ApplicationController
   end
 
   def invite
+    @executives = User.executives(@group)
     email = params[:email].try(:downcase)
-    if email.blank?
-      flash_and_render(key: :notice, message: "メールアドレスを入力してください", action: 'change')
-    elsif user = User.find_by(email: email)
+    return flash_and_render(key: :danger, message: "メールアドレスを入力してください", action: 'change') if email.blank?
+
+    if user = User.find_by(email: email)
       relationship = GroupUser.new(group_id: @group.id, user_id: user.id, role: GroupUser.roles[:general])
       if relationship.save
         NotificationMailer.invite(group: @group, user: user, current_user: current_user).deliver_later
-        flash_and_redirect(key: :notice, message: "招待に成功しました", redirect_url: change_group_url(@group))
+        flash_and_redirect(key: :success, message: "招待に成功しました", redirect_url: change_group_url(@group))
       else
-        flash_and_render(key: :notice, message: 'そのメールアドレスはすでに招待済みです', action: 'change')
+        flash_and_render(key: :danger, message: 'そのメールアドレスはすでに招待済みです', action: 'change')
       end
     else
-      flash_and_render(key: :notice, message: "メールアドレスは登録されていないまたは間違いがあります", action: 'change')
+      flash_and_render(key: :danger, message: "メールアドレスは登録されていないまたは間違いがあります", action: 'change')
     end
   end
 
@@ -159,6 +127,19 @@ class GroupsController < ApplicationController
         nil
       else
         User.find(user_id)
+      end
+    end
+
+    def incremental_search(partial)
+      keyword = params[:keyword]
+      user_ids = []
+      GroupUser.where(group_id: @group.id, role: GroupUser.roles[:general]).each do |relationship|
+        user_ids << User.find(relationship.user_id).id
+      end
+      members = User.where(id: user_ids)
+      @members = members.where('name LIKE :keyword OR furigana LIKE :keyword ', keyword: "%#{keyword.tr('ぁ-ん','ァ-ン')}%").order(furigana: :asc)
+      respond_to do |format|
+        format.json { render partial, json: @members }
       end
     end
 end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -27,4 +27,19 @@ class GroupUser < ApplicationRecord
   def self.executive_relationship(group:, user:)
     GroupUser.find_by(group_id: group.id, user_id: user.id, role: GroupUser.roles[:executive])
   end
+
+  def self.inherit(group:, current_user:, new_executive:)
+    executive_relationship = GroupUser.executive_relationship(group: group, user: current_user)
+    general_relationship = GroupUser.general_relationship(group: group, user: new_executive)
+    begin
+      GroupUser.transaction do
+        executive_relationship.update!(role: GroupUser.roles[:general])
+        general_relationship.update!(role: GroupUser.roles[:executive])
+      end
+      true
+    rescue => e
+      ErrorUtility.log_and_notify(e)
+      false
+    end
+  end
 end
