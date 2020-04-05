8923122d8242becf884f4e0973a0c267a838c3b9
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-05 09:22:28 +0900

    SendGridの情報を環境変数に

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'app158556599@heroku.com',
-    password: 'k78sbdv69601',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
